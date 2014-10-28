87e59b1168d4ed6c0face3c690d31f47d63c22d6
tobitobita <tobitobita.jp@gmail.com>
2014-10-28 17:01:25 +0900

    modify.

--- js/cv-common-functions.js
+++ js/cv-common-functions.js
@@ -1,3 +1,59 @@
+// --> 共通の仕組み
+var functionKeyFunctions = [];
+function emptyFunction() {
+    console.log('empty function.');
+}
+for (var i = 1; i <= 12; ++i) {
+    functionKeyFunctions['f' + i] = emptyFunction;
+}
+$(window).keydown(function (event) {
+    var result = false;
+    switch (event.keyCode) {
+        case 112:
+            functionKeyFunctions['f1']();
+            break;
+        case 113:
+            functionKeyFunctions['f2']();
+            break;
+        case 114:
+            functionKeyFunctions['f3']();
+            break;
+        case 115:
+            functionKeyFunctions['f4']();
+            break;
+        case 116:
+            functionKeyFunctions['f5']();
+            break;
+        case 117:
+            functionKeyFunctions['f6']();
+            break;
+        case 118:
+            functionKeyFunctions['f7']();
+            break;
+        case 119:
+            functionKeyFunctions['f8']();
+            break;
+        case 120:
+            functionKeyFunctions['f9']();
+            break;
+        case 121:
+            functionKeyFunctions['f10']();
+            break;
+        case 122:
+            functionKeyFunctions['f11']();
+            break;
+        case 123:
+            functionKeyFunctions['f12']();
+            break;
+        default:
+            result = true;
+            break;
+    }
+    return result;
+});
+// <-- 共通の仕組み
+
+// --> 各機能を実装した関数
 function showCreateView() {
     alert('showCreateView');
 }
@@ -28,3 +84,4 @@ function copyRow() {
 function saveData() {
     alert('saveData');
 }
+// --> 共通の仕組み
--- js/cv-functions.js
+++ js/cv-functions.js
@@ -1,53 +1,3 @@
-$(window).keydown(function (event) {
-    var result = false;
-    switch (event.keyCode) {
-        case 112:
-            functionKeyFunctions['f1']();
-            break;
-        case 113:
-            functionKeyFunctions['f2']();
-            break;
-        case 114:
-            functionKeyFunctions['f3']();
-            break;
-        case 115:
-            functionKeyFunctions['f4']();
-            break;
-        case 116:
-            functionKeyFunctions['f5']();
-            break;
-        case 117:
-            functionKeyFunctions['f6']();
-            break;
-        case 118:
-            functionKeyFunctions['f7']();
-            break;
-        case 119:
-            functionKeyFunctions['f8']();
-            break;
-        case 120:
-            functionKeyFunctions['f9']();
-            break;
-        case 121:
-            functionKeyFunctions['f10']();
-            break;
-        case 122:
-            functionKeyFunctions['f11']();
-            break;
-        case 123:
-            functionKeyFunctions['f12']();
-            break;
-        default:
-            result = true;
-            break;
-    }
-    return result;
-});
-
-function emptyFunction() {
-    console.log('empty function.');
-}
-var functionKeyFunctions = [];
 function initFunctions() {
     for (var i = 1; i <= 12; ++i) {
         var key = 'f' + i;
@@ -63,7 +13,6 @@ function initFunctions() {
         }
     }
 }
-
 $(document).ready(function () {
     initFunctions();
     for (var i = 1; i <= 12; ++i) {
