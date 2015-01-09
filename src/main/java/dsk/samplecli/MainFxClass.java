837a61f02d1916deaf256d684f1cbd89de0de9de
tobitobita <tobitobita.jp@gmail.com>
2015-01-09 18:09:51 +0900

    modify.

---
+++ src/main/java/dsk/samplecli/MainFxClass.java
@@ -0,0 +1,37 @@
+package dsk.samplecli;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author makoto
+ */
+public class MainFxClass extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction((ActionEvent event) -> {
+            System.out.println("Hello World!");
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 300, 250);
+
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/dsk/samplecli/SampleConsoleController.java
@@ -0,0 +1,17 @@
+package dsk.samplecli;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+public class SampleConsoleController implements Initializable {
+
+    @FXML
+    private TextField textfield;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }
+}
