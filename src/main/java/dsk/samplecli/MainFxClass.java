53c906dca796f83c6b0f4e8b32d8e92bcf83479f
tobitobita <tobitobita.jp@gmail.com>
2015-01-12 23:03:21 +0900

    modify.

--- src/main/java/dsk/samplecli/MainFxClass.java
+++ src/main/java/dsk/samplecli/MainFxClass.java
@@ -1,32 +1,19 @@
 package dsk.samplecli;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-/**
- *
- * @author makoto
- */
 public class MainFxClass extends Application {
 
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction((ActionEvent event) -> {
-            System.out.println("Hello World!");
-        });
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/SampleConsole.fxml"));
+        Scene scene = new Scene(root);
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-
-        Scene scene = new Scene(root, 300, 250);
-
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("Sample Console");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
--- src/main/java/dsk/samplecli/SampleConsoleController.java
+++ src/main/java/dsk/samplecli/SampleConsoleController.java
@@ -2,16 +2,31 @@ package dsk.samplecli;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class SampleConsoleController implements Initializable {
 
     @FXML
-    private TextField textfield;
+    private TextArea textArea;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+    }
+
+    @FXML
+    protected void handleTextFieldAction(ActionEvent event) {
+        TextField textField = (TextField) event.getSource();
+        this.textArea.appendText(textField.getText());
+        this.textArea.appendText("\n");
+        textField.clear();
+    }
+
+    @FXML
+    protected void handleButtonAction(ActionEvent event) {
+        this.textArea.clear();
     }
 }
