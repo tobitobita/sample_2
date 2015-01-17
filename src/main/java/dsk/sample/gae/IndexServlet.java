af71bb41862222bbeeb23292d95b32f0d9abdbcd
tobitobita <tobitobita.jp@gmail.com>
2015-01-17 16:40:51 +0900

    modify.

--- src/main/java/dsk/sample/gae/IndexServlet.java
+++ src/main/java/dsk/sample/gae/IndexServlet.java
@@ -1,5 +1,9 @@
 package dsk.sample.gae;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -12,6 +16,11 @@ public class IndexServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         PrintWriter out = res.getWriter();
-        out.println("Hello GAE!");
+        out.println("Hello GAE!<br>");
+
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        Entity employee = new Entity("Employee");
+        Key key = datastore.put(employee);
+        out.printf("%d<br>\n", key.getId());
     }
 }
