ab85a8387a2df285d935df86bc9fc060dcde7360
tobitobita <tobitobita.jp@gmail.com>
2015-03-17 23:53:42 +0900

    commit.

--- Launcher/src/main/java/dsk/launcher/Launch.java
+++ Launcher/src/main/java/dsk/launcher/Launch.java
@@ -9,31 +9,59 @@ import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleException;
 import org.osgi.framework.launch.Framework;
 import org.osgi.framework.launch.FrameworkFactory;
+import org.osgi.framework.startlevel.BundleStartLevel;
+import org.osgi.framework.startlevel.FrameworkStartLevel;
 
 public class Launch {
 
-    public void start(String[] args) throws BundleException {
+    public void start(String[] args) {
         System.out.printf("LAUNCH: %s\n", Arrays.toString(args));
         FrameworkFactory frameworkFactory = ServiceLoader.load(FrameworkFactory.class).iterator().next();
         Framework framework = frameworkFactory.newFramework(null);
-        framework.start();
+        try {
+            framework.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+        framework.adapt(FrameworkStartLevel.class).setStartLevel(3);
 
         BundleContext context = framework.getBundleContext();
         List<Bundle> bundles = new ArrayList<>();
-        bundles.add(context.installBundle("file:///Users/makoto/git/SampleOsgi/BookshelfInventoryApi/target/BookshelfInventoryApi-1.0-SNAPSHOT.jar"));
-        bundles.add(context.installBundle("file:///Users/makoto/git/SampleOsgi/BookshelfInventoryMock/target/BookshelfInventoryMock-1.0-SNAPSHOT.jar"));
-        bundles.add(context.installBundle("file:///Users/makoto/git/SampleOsgi/BookshelfService/target/BookshelfService-1.0-SNAPSHOT.jar"));
-        for (Bundle bundle : bundles) {
-            bundle.start();
+        try {
+            {
+                Bundle b = context.installBundle("file:///Users/PAPRIKA/git/SampleOsgi/BookshelfInventoryApi/target/BookshelfInventoryApi-1.0-SNAPSHOT.jar");
+                b.adapt(BundleStartLevel.class).setStartLevel(2);
+                bundles.add(b);
+            }
+
+            {
+                Bundle b = context.installBundle("file:///Users/PAPRIKA/git/SampleOsgi/BookshelfInventoryMock/target/BookshelfInventoryMock-1.0-SNAPSHOT.jar");
+                b.adapt(BundleStartLevel.class).setStartLevel(2);
+                bundles.add(b);
+            }
+
+            {
+                Bundle b = context.installBundle("file:///Users/PAPRIKA/git/SampleOsgi/BookshelfService/target/BookshelfService-1.0-SNAPSHOT.jar");
+                b.adapt(BundleStartLevel.class).setStartLevel(3);
+                bundles.add(b);
+            }
+            for (Bundle bundle : bundles) {
+                System.out.printf("START LEVEL: %d\n", bundle.adapt(BundleStartLevel.class).getStartLevel());
+                bundle.start();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                framework.stop();
+            } catch (BundleException ex) {
+                ex.printStackTrace();
+            }
         }
-        framework.stop();
     }
 
     public static void main(String[] args) {
-        try {
-            new Launch().start(args);
-        } catch (BundleException e) {
-            e.printStackTrace();
-        }
+        new Launch().start(args);
     }
 }
