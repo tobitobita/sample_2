22ff0c5ad64262acd6b5ea3b2ff84db0e3a51e90
tobitobita <tobitobita.jp@gmail.com>
2015-07-01 19:08:28 +0900

    commit.

--- src/jsx/index.jsx
+++ src/jsx/index.jsx
@@ -4,12 +4,47 @@
 'use strict';
 
 var Hello = React.createClass({
-    render: function () {
+    render() {
         return (<div className="container">Hello {this.props.name}!</div>);
     }
 })
 
+var CountUp = React.createClass({
+    getInitialState() {
+        return {
+            count: 0
+        };
+    },
+    onClickAction(event) {
+        console.log(event);
+        console.log('onClick, count:' + this.state.count);
+        this.setState({count: this.state.count + 1});
+    },
+    render() {
+        return (
+            <div>
+                <p>Count: {this.state.count}</p>
+
+                <p>
+                    <button onClick={this.onClickAction}>Up!</button>
+                </p>
+            </div>
+        );
+    }
+});
+
+var App = React.createClass({
+    render() {
+        return (
+            <div>
+                <Hello name="React"/>
+                <CountUp />
+            </div>
+        );
+    }
+});
+
 $(document).ready(function () {
     console.log('start');
-    React.render(<Hello name="React"/>, document.getElementById("app"));
+    React.render(<App />, document.getElementById('app'));
 });
